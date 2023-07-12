4272d3b326f5d95e8132b27703035e0c7cc3c11d
sdubyz <harungumusm@gmail.com>
2023-07-12 08:46:08 +0300

    database added

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 import views
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object("settings")
+app = Flask(__name__)
+app.config.from_object("settings")
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'movies'
+mysql = MySQL(app)
 
+def create_app():
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/algorithms", view_func=views.algorithms_page)
     app.add_url_rule("/rate", view_func=views.rate_page, methods =['GET', 'POST'])
@@ -18,5 +24,5 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     port = app.config.get("PORT", 5000)
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
